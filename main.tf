e32aa3e57f8483f07d6632297be32075f70d8e5f
maksimli04 <maksim.li@reagle.fi>
2026-01-22 11:16:46 +0200

    chore: removed crazy thing

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-  tags = {
-    environment = "dev"
-    source      = "Terraform"
-    owner       = "Maksim"
-  }
-}
-
-
 data "azuread_client_config" "current" {}
 
 
